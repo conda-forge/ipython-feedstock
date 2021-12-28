8f37637bd60f34d50507a74f322d62896083b98c
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-12-28 09:37:07 -0600

    add some skips for ppc, rework env vars

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -8,12 +8,13 @@ LINUX = platform.system() == "Linux"
 PYPY = "__pypy__" in sys.builtin_module_names
 PPC = "ppc" in platform.machine()
 
-COV_THRESHOLD = "0"
+COV_THRESHOLD = os.environ.get("COV_THRESHOLD")
 
 # Environment variable should be set in the meta.yaml
 MIGRATING = eval(os.environ.get("MIGRATING", "None"))
 
 PYTEST_SKIPS = []
+PYTEST_ARGS = [sys.executable, "-m", "pytest", "--pyargs", "IPython", "-vv"]
 
 if WIN:
     pass
@@ -21,17 +22,12 @@ else:
     pass
 
 if LINUX:
-    COV_THRESHOLD = "73"
     PYTEST_SKIPS += ["system_interrupt"]
 
 if PPC:
-    pass
+    PYTEST_SKIPS += ["ipython_dir_8", "audio_data"]
 
-PYTEST_ARGS = [sys.executable, "-m", "pytest", "--pyargs", "IPython", "-vv"]
-
-if PYPY:
-    pass
-else:
+if COV_THRESHOLD is not None:
     PYTEST_ARGS += [
         "--cov", "IPython", "--no-cov-on-fail", "--cov-fail-under", COV_THRESHOLD,
         "--cov-report", "term-missing:skip-covered"
@@ -44,13 +40,13 @@ elif PYTEST_SKIPS:
 
 if __name__ == "__main__":
     print("Building on Windows?", WIN)
-    print("Building on Linux?", LINUX)
-    print("Building for PyPy?", PYPY)
-    print("Is this a migration?", MIGRATING)
+    print("Building on Linux?  ", LINUX)
+    print("Building for PyPy?  ", PYPY)
 
     if MIGRATING:
         print("This is a migration, skipping test suite! Put it back later!", flush=True)
+        sys.exit(0)
     else:
-        env = dict(os.environ)
-        env["IPYTHON_TESTING_TIMEOUT_SCALE"] = "10"
-        sys.exit(subprocess.call(PYTEST_ARGS, env=env))
+        print("Running pytest with args")
+        print(PYTEST_ARGS, flush=True)
+        sys.exit(subprocess.call(PYTEST_ARGS))
