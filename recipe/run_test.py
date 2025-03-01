8bbebf86d94a4efb5742fd4d864a85320973b880
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-03-01 09:18:55 -0600

    update tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,62 +1,56 @@
 import subprocess
 import platform
-import sys
 from pathlib import Path
-import IPython
+import sys
 
 WIN = platform.system() == "Windows"
 LINUX = platform.system() == "Linux"
-PYPY = "__pypy__" in sys.builtin_module_names
-PPC = "ppc" in platform.machine()
 
-COV_THRESHOLD = 61 if WIN else 62
-
-PYTEST_SKIPS = ["decorator_skip", "pprint_heap_allocated"]
-PYTEST_ARGS = [sys.executable, "-m", "pytest", "-vv", "--color=yes", "--tb=long"]
-
-IGNORE_GLOBS = [
-    "consoleapp.py",
-    "external/*.py",
-    "sphinxext/*.py",
-    "terminal/console*.py",
-    "terminal/pt_inputhooks/*.py",
-    "utils/*.py",
+COV_THRESHOLD = 57 if WIN else 58
+PYTEST_SKIPS = [
+    "decorator_skip",
+    "pprint_heap_allocated",
 ]
-
-PYTEST_ARGS += sum([["--ignore-glob", glob] for glob in IGNORE_GLOBS], [])
-
-if WIN:
-    pass
-else:
-    pass
+UNLINK = [
+    # https://github.com/conda-forge/ipython-feedstock/pull/231
+    "test_zzz_autoreload.py",
+]
 
 if LINUX:
     PYTEST_SKIPS += ["system_interrupt"]
 
-if PPC:
-    PYTEST_SKIPS += ["ipython_dir_8", "audio_data"]
-
-if COV_THRESHOLD is not None:
-    PYTEST_ARGS += [
-        "--cov",
-        "IPython",
-        "--no-cov-on-fail",
-        "--cov-fail-under",
-        f"{COV_THRESHOLD}",
-        "--cov-report",
-        "term-missing:skip-covered",
-    ]
+PYTEST_ARGS = ["pytest", "-vv", "--color=yes", "--tb=long"]
 
 if len(PYTEST_SKIPS) == 1:
     PYTEST_ARGS += ["-k", f"not {PYTEST_SKIPS[0]}"]
 elif PYTEST_SKIPS:
     PYTEST_ARGS += ["-k", f"""not ({" or ".join(PYTEST_SKIPS)})"""]
 
-if __name__ == "__main__":
-    print("Building on Windows?      ", WIN)
-    print("Building on Linux?        ", LINUX)
-    print("Building for PyPy?        ", PYPY)
+COV = [sys.executable, "-m", "coverage"]
+COV_RUN = [*COV, "run", "--source", "IPython", "--branch", "-m", *PYTEST_ARGS]
+COV_REPORT = [
+    *COV,
+    "report",
+    "--show-missing",
+    "--skip-covered",
+    f"--fail-under={COV_THRESHOLD}",
+]
 
-    print("Running pytest with args")
-    print(PYTEST_ARGS, flush=True)
-    sys.exit(subprocess.call(PYTEST_ARGS, cwd=str(Path(IPython.__file__).parent)))
+
+def do(args: list[str]) -> int:
+    print(">>>", *args, flush=True)
+    return subprocess.call(args)
+
+
+def main() -> int:
+    print("Testing on Windows?      ", WIN)
+    print("Testing on Linux?        ", LINUX)
+    for stem in UNLINK:
+        path = (Path("tests") / stem).unlink()
+        print("... removing", path)
+
+    return any(map(do, [COV_RUN, COV_REPORT]))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
